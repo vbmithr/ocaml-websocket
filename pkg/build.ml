e78e29d4d5306f7391d02872de83d09ca5060896
Vincent Bernardoff <vb@luminar.eu.org>
2015-03-24 13:31:44 +0100

    Update the opam file.

--- pkg/build.ml
+++ pkg/build.ml
@@ -5,6 +5,7 @@
 let () =
   Pkg.describe "websocket" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
+    Pkg.lib ~exts:Exts.library "lib/lwt_io_ext";
     Pkg.lib ~exts:Exts.module_library "lib/websocket";
     Pkg.bin ~auto:true "tests/wscat";
   ]
