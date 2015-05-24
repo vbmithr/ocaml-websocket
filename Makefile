a4cd8a88afdcc4c3c5dc97018916a0330c25aa79
Vincent Bernardoff <vb@luminar.eu.org>
2015-05-24 14:58:58 +0200

    Conditional compilation.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PKG=websocket
 PREFIX=`opam config var prefix`
-BUILDOPTS=native=true native-dynlink=true
+BUILDOPTS=native=true native-dynlink=true lwt=true
 
 all: build
 
--- pkg/build.ml
+++ pkg/build.ml
@@ -2,11 +2,13 @@
 #directory "pkg"
 #use "topkg.ml"
 
+let lwt = Env.bool "lwt"
+
 let () =
   Pkg.describe "websocket" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.library "lib/websocket";
-    Pkg.lib ~exts:Exts.module_library "lib/websocket_lwt";
-    Pkg.bin ~auto:true "tests/wscat";
-    Pkg.bin ~auto:true "tests/reynir";
+    Pkg.lib ~cond:lwt ~exts:Exts.module_library "lib/websocket_lwt";
+    Pkg.bin ~cond:lwt ~auto:true "tests/wscat";
+    Pkg.bin ~cond:lwt ~auto:true "tests/reynir";
   ]
