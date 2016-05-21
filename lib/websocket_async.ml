21dd26c93d52ab2e10aaab8231554bb2a0d8071c
Vincent Bernardoff <vb@luminar.eu.org>
2016-05-21 11:34:06 +0200

    fix compilation

--- lib/websocket_async.ml
+++ lib/websocket_async.ml
@@ -39,7 +39,7 @@ let client
     ~ws_to_net
     uri =
   let drain_handshake r w =
-    let nonce = random_string |> B64.encode ~pad:true in
+    let nonce = random_string 16 |> B64.encode ~pad:true in
     let headers = Header.add_list extra_headers
         ["Upgrade"               , "websocket";
          "Connection"            , "Upgrade";
