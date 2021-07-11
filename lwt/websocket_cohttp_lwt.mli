56f44502bdc8227668b20e21cc9a05c02aa550dd
Antonin Décimo <antonin.decimo@gmail.com>
2021-07-11 17:01:39 +0200

    Small fixes for docstrings

--- lwt/websocket_cohttp_lwt.mli
+++ lwt/websocket_cohttp_lwt.mli
@@ -27,5 +27,5 @@ val upgrade_connection:
     connection request, and [incoming_handler], a function that will
     process incoming websocket frames, and returns ([response_action],
     [push_frame]) where [response_action] is used to produce a
-    [Cohttp_lwt.Server.t] and [push_frame] is used to send websocket
+    {!Cohttp_lwt.Server.t} and [push_frame] is used to send websocket
     frames to the client. *)
